9e80d64207fdeafee5f292197b340f81854e6935
Mickael Beaugrand <mickael.beaugrand90@gmail.com>
2024-05-16 15:38:32 +0200

    page timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Timer from "./components/pages/Timer";
+
 function App() {
   return (
     <>
-      <Timer initialSeconds={60} />
       <Outlet />
     </>
   );
