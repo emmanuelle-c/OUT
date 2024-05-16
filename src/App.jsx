75888ee1a9f042458a1fb453c4323f0e715f9cf1
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-05-16 16:19:20 +0200

    results page processing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
+import "./styles/global.css"
 
 function App() {
   return (
--- src/components/pages/Results.jsx
+++ src/components/pages/Results.jsx
@@ -1,15 +1,48 @@
 import Modal from "../Modal";
+import "../../styles/results.css";
 import { useState } from "react";
+import activities from "../../activities";
+import Card from "../Card";
 
 function Results() {
-    const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
+  const natureArray = activities.filter(
+    (activity) => activity.category === "nature"
+  );
+  const randomActivity = natureArray[Math.floor(Math.random() * natureArray.length)];
+  const sportArray = activities.filter(
+    (activity) => activity.category === "sport"
+  );
+  const cultureArray = activities.filter(
+    (activity) => activity.category === "culture"
+  );
   return (
-  <>
-  {isOpen &&
-  < Modal setIsOpen={setIsOpen} />
-  }
-  <p>Results</p>
-  </>);
+    <>
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
+      <h2 className="results-header">Out te propose 3 activités</h2>
+      <div>
+        {natureArray.map((activity) => (
+          <Card
+            key={activity.description}
+            activity={activity}
+          />
+        ))}
+        {sportArray.map((activity) => (
+          <Card
+            key={activity.description}
+            activity={activity}
+          />
+        ))}
+        {cultureArray.map((activity) => (
+          <Card
+            key={activity.description}
+            activity={activity}
+          />
+        ))}
+      </div>
+      <button>Voir plus</button>
+    </>
+  );
 }
 
 export default Results;
