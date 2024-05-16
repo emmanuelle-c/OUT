63882b756325adfad17bf4f0abc94cbe14a39712
Antoine <ant.delalande@gmail.com>
2024-05-16 15:17:28 +0200

    added basic sliders

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -1,6 +1,15 @@
+import { EmblaCarousel } from "../EmblaCarousel";
+
 function Search() {
+    const OPTIONS = {}
+    const SLIDE_COUNT = 3
+    const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+    
     return(
-        <p>Search</p>
+        <>
+            <p>Search</p>
+            <EmblaCarousel slides={SLIDES} options={OPTIONS}/>
+        </>
     )
 }
 
