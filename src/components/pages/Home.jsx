a5e452fb8fa3148aad256cdd1e6ac978fb0bf6da
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2024-05-16 17:14:27 +0200

    title json + home responsive

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
 function Home() {
@@ -8,17 +9,21 @@ function Home() {
         <h1 className="bienvenue">Bienvenue sur OUT</h1>
         <p className="paragraphe">
           {
-            "l'application qui t'aide à reconnecter avec le réel près de chez toi, tout simplement !"
+            "L' application qui t'aide à reconnecter avec le réel près de chez toi, tout simplement !"
           }
         </p>
       </div>
       <div className="buttons-home">
-        <button type="button" className="go-out">
-          Lancer OUT !
-        </button>
-        <button type="button" className="find-activity">
-          <p className="p-activity">Trouver une activité</p>
-        </button>
+        <Link to="./timer">
+          <button type="button" className="go-out">
+            Lancer OUT !
+          </button>
+        </Link>
+        <Link to="./search">
+          <button type="button" className="find-activity">
+            <p className="p-activity">Trouver une activité</p>
+          </button>
+        </Link>
       </div>
     </section>
   );
