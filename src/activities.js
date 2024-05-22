bdbfc96c7c6f64fb04534aa01bef2d6efca43daf
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-05-22 15:06:46 +0200

    img nature and culture sources changed

--- src/activities.js
+++ src/activities.js
@@ -127,7 +127,7 @@ const activities = [
         title: 'Musée du louvre',
         address: "Musée du Louvre, 75001 Paris",
         description: "Explorez une exposition d'art au Musée du Louvre mettant en vedette des artistes locaux, une opportunité de découvrir des talents émergents.",
-        img: "./assets/images/culture1.jpeg",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: true
     },
     {
@@ -135,7 +135,7 @@ const activities = [
         title: 'Théâtre Champs-Élysées',
         address: "Théâtre des Champs-Élysées, 75008 Paris",
         description: "Assistez à une représentation théâtrale en direct au Théâtre des Champs-Élysées, un spectacle captivant dans un cadre prestigieux.",
-        img: "./assets/images/culture2.jpeg",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: true
     },
     {
@@ -143,7 +143,7 @@ const activities = [
         title: 'Quartie Latin',
         address: "Quartier Latin, 75005 Paris",
         description: "Participez à une visite historique du Quartier Latin et découvrez l'histoire riche et les secrets de ce quartier emblématique de Paris.",
-        img: "./assets/images/culture3.webp",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -151,7 +151,7 @@ const activities = [
         title: 'Concert musique classique',
         address: "Philharmonie de Paris, 75019 Paris",
         description: "Assistez à un concert de musique classique à la Philharmonie de Paris, une expérience sonore exceptionnelle dans un cadre moderne.",
-        img: "./assets/images/culture1.jpeg",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -159,7 +159,7 @@ const activities = [
         title: 'Séance de dédicaces',
         address: "Bibliothèque nationale de France, 75013 Paris",
         description: "Participez à un événement de lecture et de dédicace à la Bibliothèque nationale de France, une occasion de rencontrer des auteurs et d'explorer des œuvres littéraires.",
-        img: "./assets/images/culture2.jpeg",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -167,7 +167,7 @@ const activities = [
         title: 'Festival culturel',
         address: "La Villette, 75019 Paris",
         description: "Venez profiter d'un festival culturel à La Villette avec de la nourriture, de la musique et des performances artistiques, une fête pour tous les sens.",
-        img: "./assets/images/culture3.webp",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -175,7 +175,7 @@ const activities = [
         title: 'Projection film Cinémathèque',
         address: "Cinémathèque Française, 75012 Paris",
         description: "Assistez à une projection de film suivie d'une discussion à la Cinémathèque Française, un rendez-vous pour les cinéphiles.",
-        img: "./assets/images/culture1.jpeg",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -183,7 +183,7 @@ const activities = [
         title: 'Conférence enjeux contemporains',
         address: "Sorbonne Université, 75005 Paris",
         description: "Assistez à une conférence publique à la Sorbonne Université sur les enjeux contemporains, animée par des experts et des universitaires.",
-        img: "./assets/images/culture2.jpeg",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -191,7 +191,7 @@ const activities = [
         title: 'Inauguration exposition',
         address: "Galerie Perrotin, 75003 Paris",
         description: "Découvrez l'inauguration d'une nouvelle exposition à la Galerie Perrotin, mettant en avant le travail d'un artiste contemporain émergent.",
-        img: "./assets/images/culture3.webp",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -199,7 +199,7 @@ const activities = [
         title: 'Performance de ballet',
         address: "Opéra Garnier, 75009 Paris",
         description: "Assistez à une performance de ballet à l'Opéra Garnier, un spectacle de danse classique dans un cadre somptueux.",
-        img: "./assets/images/culture1.jpeg",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -207,7 +207,7 @@ const activities = [
         title: 'Soirée opéra',
         address: "Opéra Bastille, 75012 Paris",
         description: "Profitez d'une soirée d'opéra à l'Opéra Bastille, avec des arias célèbres interprétées par des chanteurs de renommée internationale.",
-        img: "./assets/images/culture2.jpeg",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -215,7 +215,7 @@ const activities = [
         title: 'Exposition scientifique',
         address: "Cité des Sciences et de l'Industrie, 75019 Paris",
         description: "Visitez une exposition scientifique sur l'exploration spatiale à la Cité des Sciences et de l'Industrie, une aventure éducative pour tous les âges.",
-        img: "./assets/images/culture3.webp",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -223,7 +223,7 @@ const activities = [
         title: 'Foire artisanale',
         address: "Marché des Enfants Rouges, 75003 Paris",
         description: "Parcourez la foire artisanale au Marché des Enfants Rouges et découvrez des produits faits main uniques et créatifs.",
-        img: "./assets/images/culture1.jpeg",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -231,7 +231,7 @@ const activities = [
         title: 'Débat sur les enjeux politiques',
         address: "Maison de la Radio, 75016 Paris",
         description: "Assistez à un débat sur les enjeux politiques actuels à la Maison de la Radio, animé par des journalistes et des experts.",
-        img: "./assets/images/culture2.jpeg",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -239,7 +239,7 @@ const activities = [
         title: 'Défilé de mode',
         address: "Palais de Tokyo, 75116 Paris",
         description: "Assistez à un défilé de mode au Palais de Tokyo, mettant en vedette des créations de designers locaux et internationaux.",
-        img: "./assets/images/culture3.webp",
+        img: "https://images.unsplash.com/photo-1572953109213-3be62398eb95?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -247,7 +247,7 @@ const activities = [
         title: 'Promenade dans le Jardin des Plantes',
         address: "Jardin des Plantes, 75005 Paris",
         description: "Promenez-vous dans le Jardin des Plantes, un vaste jardin botanique avec une grande variété de plantes, serres tropicales, et une ménagerie.",
-        img: "./assets/images/nature1.jpeg",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: true
     },
     {
@@ -255,7 +255,7 @@ const activities = [
         title: 'Exploration du bois du boulogne',
         address: "Bois de Boulogne, 75016 Paris",
         description: "Explorez le Bois de Boulogne, un immense parc urbain offrant des sentiers de randonnée, des lacs pour faire du canotage, et des jardins paysagers.",
-        img: "./assets/images/nature2.jpeg",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: true
     },
     {
@@ -263,7 +263,7 @@ const activities = [
         title: 'Détente au Parc des Buttes-Chaumont',          
         address: "Parc des Buttes-Chaumont, 75019 Paris",
         description: "Détendez-vous au Parc des Buttes-Chaumont, un parc pittoresque avec des falaises escarpées, des cascades, et des vues panoramiques sur la ville.",
-        img: "./assets/images/nature3.webp",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -271,7 +271,7 @@ const activities = [
         title: 'Visite du Parc Monceau',
         address: "Parc Monceau, 75008 Paris",
         description: "Visitez le Parc Monceau, un parc élégant du 19ème siècle avec des statues, des ponts et un grand étang, idéal pour une promenade paisible.",
-        img: "./assets/images/nature1.jpeg",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -279,7 +279,7 @@ const activities = [
         title: 'Découvrir le Bois de Vincennes',
         address: "Bois de Vincennes, 75012 Paris",
         description: "Découvrez le Bois de Vincennes, le plus grand espace vert de Paris, offrant des sentiers de randonnée, un zoo, des jardins botaniques et un lac pour faire du bateau.",
-        img: "./assets/images/nature2.jpeg",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -287,7 +287,7 @@ const activities = [
         title: 'Espaces verts du Parc de la Vilette',
         address: "Parc de la Villette, 75019 Paris",
         description: "Profitez des espaces verts du Parc de la Villette, avec ses jardins thématiques, ses installations artistiques et ses événements en plein air.",
-        img: "./assets/images/nature3.webp",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -295,7 +295,7 @@ const activities = [
         title: 'Jardin du Luxembourg',
         address: "Jardin du Luxembourg, 75006 Paris",
         description: "Promenez-vous dans le Jardin du Luxembourg, célèbre pour ses magnifiques parterres de fleurs, ses sculptures et son grand bassin central.",
-        img: "./assets/images/nature1.jpeg",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -303,7 +303,7 @@ const activities = [
         title: 'Parc de Bercy',
         address: "Parc de Bercy, 75012 Paris",
         description: "Explorez le Parc de Bercy, un espace vert moderne avec des jardins à thème, des vignes, et des aires de jeux pour enfants.",
-        img: "./assets/images/nature2.jpeg",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -311,7 +311,7 @@ const activities = [
         title: 'Découvrir le Parc André Citroën',
         address: "Parc André Citroën, 75015 Paris",
         description: "Découvrez le Parc André Citroën, un parc contemporain avec des serres tropicales, des jardins en mouvement et une vue panoramique depuis le ballon captif.",
-        img: "./assets/images/nature3.webp",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -319,7 +319,7 @@ const activities = [
         title: "Visiter le Jardin d'Acclimatation",
         address: "Jardin d'Acclimatation, 75116 Paris",
         description: "Visitez le Jardin d'Acclimatation, un parc d'attractions historique avec des jardins, des manèges et des activités pour toute la famille.",
-        img: "./assets/images/nature1.jpeg",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -327,7 +327,7 @@ const activities = [
         title: 'Détendez-vous au Square du Vert-Galant',
         address: "Square du Vert-Galant, 75001 Paris",
         description: "Détendez-vous au Square du Vert-Galant, un petit jardin charmant situé à la pointe de l'Île de la Cité, offrant une vue imprenable sur la Seine.",
-        img: "./assets/images/nature2.jpeg",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -335,7 +335,7 @@ const activities = [
         title: 'Explorez le Parc Floral de Paris',
         address: "Parc Floral de Paris, 75012 Paris",
         description: "Explorez le Parc Floral de Paris, un grand jardin botanique avec des collections de plantes, des concerts en plein air et des aires de jeux.",
-        img: "./assets/images/nature3.webp",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -343,7 +343,7 @@ const activities = [
         title: 'Promenez-vous dans le Jardin des Tuileries',
         address: "Jardin des Tuileries, 75001 Paris",
         description: "Promenez-vous dans le Jardin des Tuileries, un parc historique entre le Louvre et la Place de la Concorde, avec des statues, des bassins et des allées ombragées.",
-        img: "./assets/images/nature1.jpeg",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -351,7 +351,7 @@ const activities = [
         title: 'Détente au Parc Montsouris',
         address: "Parc Montsouris, 75014 Paris",
         description: "Détendez-vous au Parc Montsouris, un grand parc avec des pelouses, des lacs artificiels et des sentiers ombragés, parfait pour un pique-nique.",
-        img: "./assets/images/nature2.jpeg",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     },
     {
@@ -359,7 +359,7 @@ const activities = [
         title: 'Vues panoramiques sur Paris',
         address: "Parc de Belleville, 75020 Paris",
         description: "Profitez des vues panoramiques sur Paris depuis le Parc de Belleville, un parc en terrasses avec des jardins, des aires de jeux et des œuvres d'art.",
-        img: "./assets/images/nature3.webp",
+        img: "https://images.unsplash.com/photo-1585938389612-a552a28d6914?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         popular: false
     }
     ]
